2db17324f67799065c21e8d92aa06e85db0a0d4a
AkaHanshan <tenlp@foxmail.com>
2023-04-12 23:56:14 +0800

    add static build pages

---
+++ docs/_next/static/z4FsdAtrUzOoGuq1miiiU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a6086129adba8575.css","static/chunks/pages/index-ec62a7ebf2bc624a.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  distDir: 'docs',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
