6df357809cd97c4416e6a15c50d5561a7d553ec9
AkaHanshan <tenlp@foxmail.com>
2023-04-13 14:56:01 +0800

    publish deploy to server

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  output: 'export',
-  distDir: 'docs',
-  images: {
-    unoptimized: true,
-  },
+  output: 'standalone',
 }
 
 module.exports = nextConfig
